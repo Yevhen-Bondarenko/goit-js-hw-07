0e76526d52b44c3ee9a93892f3e8b83aef564382
Yevhen-Bondarenko <bondarenko9420@gmail.com>
2024-02-19 18:25:30 +0200

    ++

--- js/task-1.js
+++ js/task-1.js
@@ -1,63 +1,13 @@
-const link1 = document.createElement('link');
-link1.rel = 'preconnect';
-link1.href = 'https://fonts.googleapis.com';
+const categoryList = document.getElementById('categories');
 
-const link2 = document.createElement('link');
-link2.rel = 'preconnect';
-link2.href = 'https://fonts.gstatic.com';
-link2.crossOrigin = '';
+const categoryItems = categoryList.querySelectorAll('.item');
 
-const link3 = document.createElement('link');
-link3.href =
-  'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap';
-link3.rel = 'stylesheet';
+console.log(`Number of categories: ${categoryItems.length}`);
 
-const head = document.head;
+categoryItems.forEach(category => {
+    const categoryName = category.querySelector('h2').textContent;
+    const categoryTotalItems = category.querySelectorAll('ul li').length;
 
-head.appendChild(link1);
-head.appendChild(link2);
-head.appendChild(link3);
-
-const input = document.querySelector('input');
-const createBtn = document.querySelector('[data-create]');
-const destroyBtn = document.querySelector('[data-destroy]');
-const boxesContainer = document.getElementById('boxes');
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, '0')}`;
-}
-
-function createBoxes(amount) {
-  const baseSize = 30;
-  const fragment = document.createDocumentFragment();
-
-  for (let i = 0; i < amount; i++) {
-    const size = baseSize + i * 10;
-    const div = document.createElement('div');
-    div.style.width = `${size}px`;
-    div.style.height = `${size}px`;
-    div.style.backgroundColor = getRandomHexColor();
-    fragment.appendChild(div);
-  }
-
-  boxesContainer.appendChild(fragment);
-}
-
-function destroyBoxes() {
-  boxesContainer.innerHTML = '';
-}
-
-createBtn.addEventListener('click', () => {
-  const amount = parseInt(input.value);
-
-  if (amount >= 1 && amount <= 100) {
-    createBoxes(amount);
-    input.value = '';
-  } else {
-    alert('Please enter a number between 1 and 100.');
-  }
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${categoryTotalItems}`);
 });
-
-destroyBtn.addEventListener('click', destroyBoxes);
